7f32ca722dba0623bc06057f2cde0ca8391a5e1d
Devin Garcia <devingarcia5423@gmail.com>
2023-08-18 10:09:21 -0400

    fix(prisma):add image to response model + seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -75,6 +75,8 @@ const main = async () => {
 					pollId: 1,
 					option: 'three',
 					response: 'Mint Chip',
+					image:
+						'https://images.unsplash.com/photo-1496041877055-c55fc243fc32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
 				},
 			},
 			account: {
