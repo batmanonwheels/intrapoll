b606c3373520a3e4cf4cbb44c6ddd127043f7ad7
Devin Garcia <devingarcia5423@gmail.com>
2023-08-18 10:04:44 -0400

    fix(header): capitalize header text

--- src/components/(header)/Header.tsx
+++ src/components/(header)/Header.tsx
@@ -8,7 +8,7 @@ const Header = async ({}: HeaderProps) => {
 		<header className='fixed top-0 min-w-full min-h-fit flex justify-between px-2 pt-2 bg-inherit z-10 mb-0'>
 			<Link href={'/'}>
 				<h1 className='text-3xl font-extrabold tracking-tight lg:text-5xl'>
-					interpoll
+					INTERPOLL
 				</h1>
 			</Link>
 			<NavMenu />
